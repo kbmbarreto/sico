5fa938f6225b35a85eade05de69b6e013779c67e
Kleber Barreto de Macedo <kleber.barreto@msn.com>
2021-03-17 01:49:15 -0300

    Corrigida a visualização da tabela por ordem decrescente

--- src/main/java/br/com/kmacedo/sico/controller/ApontamentoController.java
+++ src/main/java/br/com/kmacedo/sico/controller/ApontamentoController.java
@@ -24,7 +24,7 @@ public class ApontamentoController {
 	// display list of apontamentos
 	@GetMapping("/apontamentos")
 	public String viewApontamentoPage(Model model) {
-		return findPaginated(1, "data", "asc", model);		
+		return findPaginated(1, "data", "desc", model);		
 	}
 	
 	@GetMapping("/showNewApontamentoForm")
